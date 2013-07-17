5e0b6e8a6d290b6b9cff5e43d8e634cb0db1a11c
Scott Ramsden <scott@scottramsden.co.uk>
2013-07-17 22:27:32 +0100

    Added the basic layout of the FYP-Panel

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>FYP - <?php echo $companyName;?></title>
+		<link rel="stylesheet" type="text/css" href="main.css">
+	</head>
+<body>
+<script>
+var screenWidth = window.screen.width;
+</script>
+<div id="container">
+	<div id="header">
+		<img src="http://placehold.it/350x150&text=FYP"/>
+	</div>
+	<div id="menu">
+		<ul>
+			<li>Item One</li>
+				<li>Item One</li>
+<li>Item One</li>
+			<li>Item Two</li>
+		</ul>
+		<div style="clear:both;"></div>
+	</div>
+	<div id="footer">
+	Footer Text Here
+	</div>
+</div> 
+</body>
+</html>
