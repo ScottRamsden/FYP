9e2cd8dff2cdde6f40f55f4bcdccfdef397488ab
Scott Ramsden <scott@scottramsden.co.uk>
2014-02-20 17:32:14 +0000

    Read vars from files instead of static array for power

--- functions/getPowerStatistics.php
+++ functions/getPowerStatistics.php
@@ -1,30 +1,5 @@
 <?php
 
-
-$hour = array(
-    'pound' => 11,
-    'joules' => 15,
-    'status' => 'Good'
-);
-
-$week = array(
-    'pound' => 11,
-    'joules' => 15,
-    'status' => 'Good'
-);
-
-$month = array(
-    'pound' => 11,
-    'joules' => 15,
-    'status' => 'Good'
-);
-
-$yesterday = array(
-    'pound' => 11,
-    'joules' => 15,
-    'status' => 'Good'
-);
-
 // Today
 $filename = '/fyp/power/today';
 $fp = @fopen($filename, 'r');
@@ -52,3 +27,110 @@ if ($fp) {
     }
 }
 
+// Hour
+$filename = '/fyp/power/hour';
+$fp = @fopen($filename, 'r');
+
+if ($fp) {
+    $array = explode("\n", fread($fp, filesize($filename)));
+
+    $hour = array();
+    $count = 1;
+    foreach ($array as $item) {
+        if (!empty($item)) {
+            if ($count == 1) {
+                $hour['pound'] = $item;
+            }
+            if ($count == 2) {
+                $hour['joules'] = $item;
+            }
+            $count = 2;
+        }
+    }
+    if ($hour['joules'] > 5) {
+        $hour['status'] = 'Good';
+    } else {
+        $hour['status'] = 'Bad';
+    }
+}
+
+// Week
+$filename = '/fyp/power/week';
+$fp = @fopen($filename, 'r');
+
+if ($fp) {
+    $array = explode("\n", fread($fp, filesize($filename)));
+
+    $week = array();
+    $count = 1;
+    foreach ($array as $item) {
+        if (!empty($item)) {
+            if ($count == 1) {
+                $week['pound'] = $item;
+            }
+            if ($count == 2) {
+                $week['joules'] = $item;
+            }
+            $count = 2;
+        }
+    }
+    if ($week['joules'] > 5) {
+        $week['status'] = 'Good';
+    } else {
+        $week['status'] = 'Bad';
+    }
+}
+
+// Month
+$filename = '/fyp/power/month';
+$fp = @fopen($filename, 'r');
+
+if ($fp) {
+    $array = explode("\n", fread($fp, filesize($filename)));
+
+    $month = array();
+    $count = 1;
+    foreach ($array as $item) {
+        if (!empty($item)) {
+            if ($count == 1) {
+                $month['pound'] = $item;
+            }
+            if ($count == 2) {
+                $month['joules'] = $item;
+            }
+            $count = 2;
+        }
+    }
+    if ($month['joules'] > 5) {
+        $month['status'] = 'Good';
+    } else {
+        $month['status'] = 'Bad';
+    }
+}
+
+// Yesterday
+$filename = '/fyp/power/yesterday';
+$fp = @fopen($filename, 'r');
+
+if ($fp) {
+    $array = explode("\n", fread($fp, filesize($filename)));
+
+    $yesterday = array();
+    $count = 1;
+    foreach ($array as $item) {
+        if (!empty($item)) {
+            if ($count == 1) {
+                $yesterday['pound'] = $item;
+            }
+            if ($count == 2) {
+                $yesterday['joules'] = $item;
+            }
+            $count = 2;
+        }
+    }
+    if ($yesterday['joules'] > 5) {
+        $yesterday['status'] = 'Good';
+    } else {
+        $yesterday['status'] = 'Bad';
+    }
+}
