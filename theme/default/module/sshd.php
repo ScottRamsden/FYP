faf10aed670726fc9b91d6bb9450139454e3f4c9
Scott Ramsden <scott@scottramsden.co.uk>
2014-03-26 14:12:25 +0000

    Update module pages

--- theme/default/module/sshd.php
+++ theme/default/module/sshd.php
@@ -1,3 +1,28 @@
-The service is running, so you can connect using your prefered method
+<p>The service is running, so you can connect using your prefered method. Please download a client or use the linux terminal application</p>
+<hr/>
+<div style="float:left;margin-right:15px;">
+    <img src="http://www.unixwiz.net/images/putty-openssh-3.gif" width="150px" height="150px"/>
+</div>
+<div style="float:left;width:590px">
+    <p>
+        PuTTY is a free implementation of Telnet and SSH for Windows and Unix platforms, along with an xterm terminal emulator.
+    </p>
 
+    <a href="http://www.chiark.greenend.org.uk/~sgtatham/putty">http://www.chiark.greenend.org.uk/~sgtatham/putty</a>
+</div>
+<div style="clear:both;"></div>
+<br/>
+<hr/>
+<div style="float:left;margin-right:15px;">
+    <img src="http://linuxcommand.org/images/Screenshot-Terminal.png" width="150px" height="150px">
+</div>
+<div style="float:left;width:590px">
+    <p>
+        A text terminal, or often just terminal (sometimes text console) is a serial computer interface for text entry and display. Information is presented as an array of pre-selected formed characters.
+    </p>
+</div>
+<div style="clear:both;"></div>
+<br/>
+<hr/>
+<h2 style="text-align:center;"><?php echo $_SERVER['HTTP_HOST'];?></h2>
 <hr/>
